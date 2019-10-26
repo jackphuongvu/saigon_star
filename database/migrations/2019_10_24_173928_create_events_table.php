98ec02948f22dae42f159ae1c9c02179fe4952fc
root <root@ip-172-31-16-123.ap-southeast-1.compute.internal>
2019-10-26 08:07:36 +0000

    update

--- database/migrations/2019_10_24_173928_create_events_table.php
+++ database/migrations/2019_10_24_173928_create_events_table.php
@@ -16,11 +16,11 @@ class CreateEventsTable extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
-            $table->text('description')->default('');
+            $table->text('description');
             $table->tinyInteger('active');
             $table->string('location');
             $table->string('file_path')->nullable();
-            $table->unsignedInteger('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
