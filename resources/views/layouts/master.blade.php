c043a5ab7e34c4edbffebad203711c235e5353d8
Jack <momabz6@gmail.com>
2019-11-10 17:09:09 +0700

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,14 +4,14 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<!-- Google Tag Manager -->
 	{{--<script async src="js/fbevents.js"></script>--}}
-	<script type="text/javascript" async src="{{asset('js/analytics.js')}}"></script>
-	<script async src="{{asset('js/gtm.js')}}"></script>
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-K9V338L');
-	</script>
+	{{--<script type="text/javascript" async src="{{asset('js/analytics.js')}}"></script>--}}
+	{{--<script async src="{{asset('js/gtm.js')}}"></script>--}}
+	{{--<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':--}}
+	{{--new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],--}}
+	{{--j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=--}}
+	{{--'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);--}}
+	{{--})(window,document,'script','dataLayer','GTM-K9V338L');--}}
+	{{--</script>--}}
 	<!-- End Google Tag Manager -->
 
 	<meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1, user-scalable=no">
